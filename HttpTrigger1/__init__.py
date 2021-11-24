96c66dab414602f6de934b35b1c7927624fc42b4
Phil Hamilton <phil.hamilton@jacobs.com>
2021-11-24 15:45:32 +0000

    commiting

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,30 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    path = "HttpTrigger1/example_folder/example.txt"
+    with open(path) as f:
+        lines = f.readlines()
+        print(lines)
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        # return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {lines}")
+
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
